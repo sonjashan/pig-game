91739d22d8d50c3d6ea1c2b62f193f65fcae0eb4
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-01 13:10:09 -0400

    refactor repeated code & stop counting at gameover

--- script.js
+++ script.js
@@ -15,70 +15,79 @@ let score0 = 0;
 let score1 = 0;
 let curScore0 = 0;
 let curScore1 = 0;
+let hasWinner = false;
 
 // generate the starting page
 score0Elmt.textContent = 0;
 score1Elmt.textContent = 0;
 diceImgElmt.classList.add("hidden");
 
+const switch0To1 = function () {
+    player0Elmt.classList.remove("player--active");
+    curScore0 = 0;
+    curScore0Elmt.textContent = curScore0;
+    player1Elmt.classList.add("player--active");
+};
+const switch1To0 = function () {
+    player1Elmt.classList.remove("player--active");
+    curScore1 = 0;
+    curScore1Elmt.textContent = curScore1;
+    player0Elmt.classList.add("player--active");
+};
+
+function ifActive0() {
+    return player0Elmt.classList.contains("player--active") ? true : false;
+}
+
 // generate a random dice roll
 rollBtnElmt.addEventListener("click", function () {
-    let resultRoll = Math.floor(Math.random() * 6) + 1;
-    let imgPath = `img/dice-${String(resultRoll)}.png`;
-    // console.log(resultRoll, imgPath);
-    diceImgElmt.src = imgPath;
-    diceImgElmt.classList.remove("hidden");
+    if (!hasWinner) {
+        let resultRoll = Math.floor(Math.random() * 6) + 1;
+        let imgPath = `img/dice-${String(resultRoll)}.png`;
+        // console.log(resultRoll, imgPath);
+        diceImgElmt.src = imgPath;
+        diceImgElmt.classList.remove("hidden");
 
-    if (resultRoll === 1) {
-        if (player0Elmt.classList.contains("player--active")) {
-            player0Elmt.classList.remove("player--active");
-            curScore0 = 0;
-            curScore0Elmt.textContent = curScore0;
-
-            player1Elmt.classList.add("player--active");
+        if (resultRoll === 1) {
+            if (ifActive0()) {
+                switch0To1();
+            } else {
+                switch1To0();
+            }
         } else {
-            player1Elmt.classList.remove("player--active");
-            curScore1 = 0;
-            curScore1Elmt.textContent = curScore1;
-
-            player0Elmt.classList.add("player--active");
-        }
-    } else {
-        if (player0Elmt.classList.contains("player--active")) {
-            curScore0 += resultRoll;
-            curScore0Elmt.textContent = curScore0;
-        } else {
-            curScore1 += resultRoll;
-            curScore1Elmt.textContent = curScore1;
+            if (ifActive0()) {
+                curScore0 += resultRoll;
+                curScore0Elmt.textContent = curScore0;
+            } else {
+                curScore1 += resultRoll;
+                curScore1Elmt.textContent = curScore1;
+            }
         }
     }
 });
 
 // the hold button
 holdBtnElmt.addEventListener("click", function () {
-    if (player0Elmt.classList.contains("player--active")) {
-        score0 += curScore0;
-        score0Elmt.textContent = score0;
-        if (score0 >= 100) {
-            player0Elmt.classList.add("player--winner");
+    if (!hasWinner) {
+        if (ifActive0()) {
+            score0 += curScore0;
+            score0Elmt.textContent = score0;
+            if (score0 >= 100) {
+                player0Elmt.classList.add("player--winner");
+                hasWinner = true;
+            } else {
+                switch0To1();
+            }
         } else {
-            // switch
-            player0Elmt.classList.remove("player--active");
-            curScore0 = 0;
-            curScore0Elmt.textContent = curScore0;
-            player1Elmt.classList.add("player--active");
-        }
-    } else {
-        // player 1 active
-        score1 += curScore1;
-        score1Elmt.textContent = score1;
-        if (score1 >= 100) {
-            player1Elmt.classList.add("player--winner");
-        } else {
-            player1Elmt.classList.remove("player--active");
-            curScore1 = 0;
-            curScore1Elmt.textContent = curScore1;
-            player0Elmt.classList.add("player--active");
+            // player 1 active
+            score1 += curScore1;
+            score1Elmt.textContent = score1;
+            if (score1 >= 100) {
+                player1Elmt.classList.add("player--winner");
+                hasWinner = true;
+            } else {
+                switch1To0();
+            }
         }
     }
 });
