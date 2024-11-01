605ad933362ba23955fe76bda67c0eb5105fa0de
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-01 15:23:49 -0400

    refactor: use array, template literals to simplify

--- script.js
+++ script.js
@@ -11,57 +11,47 @@ let curScore1Elmt = document.getElementById("current--1");
 let holdBtnElmt = document.querySelector(".btn--hold");
 let newBtnElmt = document.querySelector(".btn--new");
 
-let score0 = 0;
-let score1 = 0;
-let curScore0 = 0;
-let curScore1 = 0;
-let hasWinner = false;
+let hasWinner, scores, curScore, activePlayer;
 
-// generate the starting page
-score0Elmt.textContent = 0;
-score1Elmt.textContent = 0;
-diceImgElmt.classList.add("hidden");
+function init() {
+    hasWinner = false;
+    scores = [0, 0];
+    curScore = 0;
+    activePlayer = 0;
+    diceImgElmt.classList.add("hidden");
 
-const switch0To1 = function () {
-    player0Elmt.classList.remove("player--active");
-    curScore0 = 0;
-    curScore0Elmt.textContent = curScore0;
-    player1Elmt.classList.add("player--active");
-};
-const switch1To0 = function () {
-    player1Elmt.classList.remove("player--active");
-    curScore1 = 0;
-    curScore1Elmt.textContent = curScore1;
-    player0Elmt.classList.add("player--active");
-};
+    score0Elmt.textContent = 0;
+    score1Elmt.textContent = 0;
+    curScore0Elmt.textContent = 0;
+    curScore1Elmt.textContent = 0;
 
-function ifActive0() {
-    return player0Elmt.classList.contains("player--active") ? true : false;
+    player0Elmt.classList.remove("player--winner");
+    player1Elmt.classList.remove("player--winner");
 }
+
+init();
+
+const switchPlayer = function () {
+    curScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent = curScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0Elmt.classList.toggle("player--active");
+    player1Elmt.classList.toggle("player--active");
+};
 
 // generate a random dice roll
 rollBtnElmt.addEventListener("click", function () {
     if (!hasWinner) {
         let resultRoll = Math.floor(Math.random() * 6) + 1;
-        let imgPath = `img/dice-${String(resultRoll)}.png`;
-        // console.log(resultRoll, imgPath);
-        diceImgElmt.src = imgPath;
+        diceImgElmt.src = `img/dice-${String(resultRoll)}.png`;
         diceImgElmt.classList.remove("hidden");
 
         if (resultRoll === 1) {
-            if (ifActive0()) {
-                switch0To1();
-            } else {
-                switch1To0();
-            }
+            switchPlayer();
         } else {
-            if (ifActive0()) {
-                curScore0 += resultRoll;
-                curScore0Elmt.textContent = curScore0;
-            } else {
-                curScore1 += resultRoll;
-                curScore1Elmt.textContent = curScore1;
-            }
+            // update the score
+            curScore += resultRoll;
+            document.getElementById(`current--${activePlayer}`).textContent = curScore;
         }
     }
 });
@@ -69,51 +59,20 @@ rollBtnElmt.addEventListener("click", function () {
 // the hold button
 holdBtnElmt.addEventListener("click", function () {
     if (!hasWinner) {
-        if (ifActive0()) {
-            score0 += curScore0;
-            score0Elmt.textContent = score0;
-            if (score0 >= 100) {
-                player0Elmt.classList.add("player--winner");
-                hasWinner = true;
-            } else {
-                switch0To1();
-            }
+        scores[activePlayer] += curScore;
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        if (scores[activePlayer] >= 100) {
+            // one side has won, game is finished
+            document.querySelector(`.player--${activePlayer}`).classList.add("player--winner");
+            hasWinner = true;
         } else {
-            // player 1 active
-            score1 += curScore1;
-            score1Elmt.textContent = score1;
-            if (score1 >= 100) {
-                player1Elmt.classList.add("player--winner");
-                hasWinner = true;
-            } else {
-                switch1To0();
-            }
+            switchPlayer();
         }
     }
 });
 
 // the new game button
 newBtnElmt.addEventListener("click", function () {
-    hasWinner = false;
-    diceImgElmt.classList.add("hidden");
-    score0 = 0;
-    score1 = 0;
-    curScore0 = 0;
-    curScore1 = 0;
-    score0Elmt.textContent = score0;
-    score1Elmt.textContent = score1;
-    curScore0Elmt.textContent = curScore0;
-    curScore1Elmt.textContent = curScore1;
-    if (player0Elmt.classList.contains("player--winner")) {
-        player0Elmt.classList.remove("player--winner");
-    } else {
-        player1Elmt.classList.remove("player--winner");
-        switch1To0();
-    }
+    if (activePlayer === 1) switchPlayer();
+    init();
 });
-
-
-
-
-
-
