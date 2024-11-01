fd0ef1cf8193b67504f5455a98e0c4c67d24b6b4
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-10-31 22:31:38 -0400

    set up starting page and dice rolls

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 'use strict';
+
+let score0Elmt = document.getElementById("score--0");
+let score1Elmt = document.querySelector("#score--1");
+let diceImgElmt = document.querySelector(".dice");
+let rollBtnElmt = document.querySelector(".btn--roll");
+let player0Elmt = document.querySelector(".player--0");
+let player1Elmt = document.querySelector(".player--1");
+let curScore0Elmt = document.getElementById("current--0");
+let curScore1Elmt = document.getElementById("current--1");
+
+let curScore0 = 0;
+let curScore1 = 0;
+
+// generate the starting page
+score0Elmt.textContent = 0;
+score1Elmt.textContent = 0;
+diceImgElmt.classList.add("hidden");
+
+// generate a random dice roll
+rollBtnElmt.addEventListener("click", function () {
+    let resultRoll = Math.floor(Math.random() * 6) + 1;
+    let imgPath = `img/dice-${String(resultRoll)}.png`;
+    // console.log(resultRoll, imgPath);
+    diceImgElmt.src = imgPath;
+    diceImgElmt.classList.remove("hidden");
+
+    if (resultRoll === 1) {
+        if (player0Elmt.classList.contains("player--active")) {
+            player0Elmt.classList.remove("player--active");
+            curScore0 = 0;
+            curScore0Elmt.textContent = curScore0;
+
+            player1Elmt.classList.add("player--active");
+        } else {
+            player1Elmt.classList.remove("player--active");
+            curScore1 = 0;
+            curScore1Elmt.textContent = curScore1;
+
+            player0Elmt.classList.add("player--active");
+        }
+    } else {
+        if (player0Elmt.classList.contains("player--active")) {
+            curScore0 += resultRoll;
+            curScore0Elmt.textContent = curScore0;
+        } else {
+            curScore1 += resultRoll;
+            curScore1Elmt.textContent = curScore1;
+        }
+    }
+});
+
+
+
+
+
+
+
+
+
