61110265b048f5531b6c85fc80023b70f396b080
sonjashan <102500611+sonjashan@users.noreply.github.com>
2024-11-01 12:22:41 -0400

    implement the hold button

--- script.js
+++ script.js
@@ -8,7 +8,11 @@ let player0Elmt = document.querySelector(".player--0");
 let player1Elmt = document.querySelector(".player--1");
 let curScore0Elmt = document.getElementById("current--0");
 let curScore1Elmt = document.getElementById("current--1");
+let holdBtnElmt = document.querySelector(".btn--hold");
+let newBtnElmt = document.querySelector(".btn--new");
 
+let score0 = 0;
+let score1 = 0;
 let curScore0 = 0;
 let curScore1 = 0;
 
@@ -50,8 +54,34 @@ rollBtnElmt.addEventListener("click", function () {
     }
 });
 
-
-
+// the hold button
+holdBtnElmt.addEventListener("click", function () {
+    if (player0Elmt.classList.contains("player--active")) {
+        score0 += curScore0;
+        score0Elmt.textContent = score0;
+        if (score0 >= 100) {
+            player0Elmt.classList.add("player--winner");
+        } else {
+            // switch
+            player0Elmt.classList.remove("player--active");
+            curScore0 = 0;
+            curScore0Elmt.textContent = curScore0;
+            player1Elmt.classList.add("player--active");
+        }
+    } else {
+        // player 1 active
+        score1 += curScore1;
+        score1Elmt.textContent = score1;
+        if (score1 >= 100) {
+            player1Elmt.classList.add("player--winner");
+        } else {
+            player1Elmt.classList.remove("player--active");
+            curScore1 = 0;
+            curScore1Elmt.textContent = curScore1;
+            player0Elmt.classList.add("player--active");
+        }
+    }
+});
 
 
 
